f5d8e07ab5b4c1fa1f29e25a4f5c90ced8c6f092
rtyanas <rtyanas2000@yahoo.com>
2013-12-22 12:28:10 -0500

    initial commit

---
+++ workspace/MyPic/src/com/yanas/mobileapp/mypic/GlobalSettings.java
@@ -0,0 +1,15 @@
+package com.yanas.mobileapp.mypic;
+
+public class GlobalSettings {
+
+	public static boolean mainActivity = true;
+
+	public static boolean dataStore = true;
+	
+	public static boolean getImageActivity = true;
+	
+	public static boolean settingsActivity = true;
+
+	public static boolean messageData = true;
+	
+}
