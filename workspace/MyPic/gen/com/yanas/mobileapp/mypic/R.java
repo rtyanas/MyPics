82708acf4d4ccacdc68b50c0dc64b07adac987ac
rtyanas <rtyanas2000@yahoo.com>
2014-01-14 12:50:00 -0500

    Add resource text_icon and action_settings_menu_item

--- workspace/MyPic/gen/com/yanas/mobileapp/mypic/R.java
+++ workspace/MyPic/gen/com/yanas/mobileapp/mypic/R.java
@@ -31,11 +31,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int ic_launcher_camera=0x7f020001;
         public static final int ic_launcher_richeall=0x7f020002;
+        public static final int text_icon=0x7f020003;
     }
     public static final class id {
         public static final int Button01=0x7f090000;
         public static final int ImageView01=0x7f090001;
         public static final int action_settings=0x7f090018;
+        public static final int action_settings_menu_item=0x7f090019;
         public static final int backGroundButton=0x7f090017;
         public static final int color=0x7f090010;
         public static final int done=0x7f090009;
